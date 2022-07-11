4d5cbb3eb903ccf102c91e0da3396c31958f2e37
HP <sachdevamohita5@gmail.com>
2022-07-11 11:30:02 +0530

    harmonic number

---
+++ src/bridgelap/com/HarmonicNumber.java
@@ -0,0 +1,15 @@
+package bridgelap.com;
+
+    public class HarmonicNumber {
+
+        public static void main(String[] args) {
+            int num = 5;
+            double result = 0.0;
+            for (int i = num; i > 0; i--) {
+                result = result + (double) 1 / num;
+                System.out.println("result-->" + result);
+            }
+        }
+    }
+
+
